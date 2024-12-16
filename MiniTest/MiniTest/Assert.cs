799feab9c1ba3de6442cf77d60b5c15f8092e776
srebrek <zlotekmikolaj@gmail.com>
2024-12-16 16:02:02 +0100

    MiniTest library done but not tested

---
+++ MiniTest/MiniTest/Assert.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniTest
+{
+    public static class Assert
+    {
+        public static void ThrowsException<TException>(Action action, string message = "") where TException : Exception
+        {
+            try
+            {
+                action();
+            }
+            catch (TException)
+            {
+                return;
+            }
+            catch (Exception ex)
+            {
+                throw new AssertionException($"Expected exception of type:<{typeof(TException)}>. Actual exception type:<{ex.GetType()}>. {message}");
+            }
+            throw new AssertionException($"Expected exception of type:<{typeof(TException)}> but no exception was thrown. {message}");
+        }
+
+        public static void AreEqual<T>(T? expected, T? actual, string message = "")
+        {
+            if (expected != null && expected.Equals(actual))
+                return;
+            if (expected == null && actual == null)
+                return;
+            throw new AssertionException($"Expected: {expected?.ToString()}. Actual: {actual?.ToString()}. {message}");
+        }
+
+        public static void AreNotEqual<T>(T? expected, T? actual, string message = "")
+        {
+            if (expected != null && !expected.Equals(actual))
+                return;
+            if (expected == null && actual != null)
+                return;
+            throw new AssertionException($"Expected any value except: {expected?.ToString()}. Actual: {actual?.ToString()}. {message}");
+        }
+
+        public static void IsTrue(bool condition, string message = "")
+        {
+            if (condition)
+                return;
+            throw new AssertionException($"Expected: True. Actual: False. {message}");
+        }
+
+        public static void IsFalse(bool condition, string message = "")
+        {
+            if (!condition)
+                return;
+            throw new AssertionException($"Expected: False. Actual: True. {message}");
+        }
+
+        public static void Fail(string message = "")
+        {
+            throw new AssertionException($"Fail. {message}");
+        }
+    }
+}
---
+++ MiniTest/MiniTest/TestAttributes.cs
@@ -0,0 +1,57 @@
+﻿namespace MiniTest
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class TestClassAttribute : Attribute
+    {
+    }
+
+    [AttributeUsage(AttributeTargets.Method)]
+    public class TestMethodAttribute : Attribute
+    {
+    }
+
+    [AttributeUsage(AttributeTargets.Method)]
+    public class BeforeEachAttribute : Attribute
+    {
+    }
+
+    [AttributeUsage(AttributeTargets.Method)]
+    public class AfterEachAttribute : Attribute
+    {
+    }
+
+    [AttributeUsage(AttributeTargets.Method)]
+    public class PriorityAttribute : Attribute
+    {
+        public int Priority { get; }
+
+        public PriorityAttribute(int priority)
+        {
+            Priority = priority;
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+    public class DataRowAttribute : Attribute
+    {
+        public object?[] Data { get; }
+        public string? Description { get; }
+
+        public DataRowAttribute(object?[] data, string? description = null)
+        {
+            Data = data;
+            Description = description;
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class DescriptionAttribute : Attribute
+    {
+        public string Description { get; }
+
+        public DescriptionAttribute(string description)
+        {
+            Description = description;
+        }
+    }
+}
